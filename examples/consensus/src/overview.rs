63dd9a2fe16dc77121583525586d4927724659ae
David Wong <davidwong.crypto@gmail.com>
2021-08-02 14:44:28 -0700

    this repo is the example

--- examples/consensus/src/overview.rs
+++
@@ -1,8 +0,0 @@
-//~
-//~ This document is organized as follows:
-//~ * Architecture - the components of the specification and how they interact.
-//~ * Data structures - common data structures that are part of this specification.
-//~ * Network messages - consensus messages that are sent across the wire to other validators.
-//~ * Abstracted modules - The components this specification depends on.
-//~ * Consensus modules - The components built upon common data structures that are described as a part of this specification.
-//~ * All network communication occurs over DiemNet and any serialization, deserialization and hashing is determined by BCS.
