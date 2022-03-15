2ed26a198a26e0ac2c3177aa81595d8cfa6eb0af
David Wong <davidwong.crypto@gmail.com>
2022-03-14 18:30:51 -0700

    support for ocaml should work

--- src/comment_parser.rs
+++ src/comment_parser.rs
@@ -22,20 +22,33 @@ pub fn parse_file(file_name: &Path) -> Result<String> {
             .wrap_err_with(|| format!("could not read file {}", file_name.display())),
 
         //~ - for python files we look for comments starting with `#~`
-        "py" => parse_code("#~", file_name),
+        "py" => parse_code("#~", None, file_name),
+
+        //~ - for python files we look for comments starting with `#~`
+        "ml" | "mli" => parse_code("(*~", Some("*)"), file_name),
 
         //~ - for other files we look for comments starting with `//~`
-        _ => parse_code("//~", file_name),
+        _ => parse_code("//~", None, file_name),
     }
+}
+
+/// detects if a comment ends on this same line
+fn has_end(end: &str, comment: &str) -> bool {
+    comment.trim().ends_with(end)
 }
 
 /// Parse code to return the specification-related content
 /// (comments that start with a special delimiter, by default `~`)
-pub fn parse_code(delimiter: &str, file_name: &Path) -> Result<String> {
-    // state
-    let mut extract_code = None; // indicates if we're between `//~ spec:startcode` and `//~spec:endcode` statements
+pub fn parse_code(
+    start_comment: &str,
+    end_comment: Option<&str>,
+    file_name: &Path,
+) -> Result<String> {
+    let mut extract_code = None; // set if we're waiting for an encode instruction
+    let mut in_spec_comment = false; // set if we're within a multi-line in a comment
     let mut result = String::new();
 
+    // read file
     let source = std::fs::read_to_string(file_name)
         .into_diagnostic()
         .wrap_err_with(|| format!("could not read file {}", file_name.display()))?;
@@ -44,7 +57,7 @@ pub fn parse_code(delimiter: &str, file_name: &Path) -> Result<String> {
     let mut byte_offset_for_errors = 0;
     for line in source.lines() {
         // if this is a normal line...
-        if !line.trim_start().starts_with(delimiter) {
+        if !line.trim_start().starts_with(start_comment) && !in_spec_comment {
             // only print a normal line if it is between `//~ spec:startcode` and `//~spec:endcode` statements
             if extract_code.is_some() {
                 // TODO: reset indentation
@@ -55,11 +68,11 @@ pub fn parse_code(delimiter: &str, file_name: &Path) -> Result<String> {
             continue;
         }
 
-        // if the line starts with //~ parse it
-        let comment = line.split_once(delimiter).unwrap().1;
+        //~ detect spec comment
+        let comment = line.split_once(start_comment).unwrap().1;
 
         //~ lines starting with `//~ spec:instruction` are specific instructions
-        if comment.trim().starts_with(SPECIFICATION_INSTRUCTION) {
+        if !in_spec_comment && comment.trim().starts_with(SPECIFICATION_INSTRUCTION) {
             match comment.split_once(SPECIFICATION_INSTRUCTION).unwrap().1 {
                 //~ a comment starting with `//~ spec:startcode` will print
                 //~ every line afterwards, up until a `//~ spec:endcode` statement
@@ -98,6 +111,20 @@ pub fn parse_code(delimiter: &str, file_name: &Path) -> Result<String> {
             };
         } else {
             // extract the specification text
+            let comment = if let Some(end) = end_comment {
+                if has_end(end, comment) {
+                    // either the comment is ending
+                    in_spec_comment = false;
+                    comment.trim_end_matches(end)
+                } else {
+                    // or it goes on to the next line
+                    in_spec_comment = true;
+                    comment
+                }
+            } else {
+                comment
+            };
+
             let comment = comment.strip_prefix(' ').unwrap_or(comment);
             writeln!(&mut result, "{}", comment).unwrap();
         }
